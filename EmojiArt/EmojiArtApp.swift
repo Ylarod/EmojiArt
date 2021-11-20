f7a2afdbb301e291f8fdf17769e304b790d97bbe
Ylarod <me@ylarod.cn>
2021-11-20 21:09:11 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ylarod on 2021/11/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
