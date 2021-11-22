6c259fb7ee04addaeeb8877c32706674ccda7965
Ylarod <me@ylarod.cn>
2021-11-22 20:38:48 +0800

    Lecture 13 done

--- EmojiArt/EmojiArtDocumentView.swift
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -70,6 +70,9 @@ struct EmojiArtDocumentView: View {
                     break
                 }
             }
+            onReceive(document.$backgroundImage) { image in
+                zoomToFit(image, in: geometry.size)
+            }
         }
     }
     
